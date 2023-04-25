4d45bd1344ca703263b1b17082e0ae8aa4989c91
Richard <xrouter@gmail.com>
2023-04-25 10:15:56 +0100

    Checkpoint, finially fixed global form field issue

--- maps/fields.py
+++ maps/fields.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.gis.geos import GEOSGeometry
+from django.core.exceptions import ValidationError
+from .forms import LocationFormField
+
+
 class Location(models.Field):
     description = "A custom field to store a GEOMETRY type and return GeoJSON"
 
     def __init__(self, *args, **kwargs):
         # Add any custom initialization code here
+        #kwargs['blank'] = kwargs.get('blank', True)
+        #kwargs['null'] = kwargs.get('null', True)
         super().__init__(*args, **kwargs)
 
     def db_type(self, connection):
@@ -12,4 +18,8 @@ class Location(models.Field):
 
     def from_db_value(self, value, expression, connection):
         return GEOSGeometry(value)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': LocationFormField, **kwargs}
+        return super().formfield(**defaults)
 
--- maps/widgets.py
+++ maps/widgets.py
@@ -1,9 +1,20 @@
-from django import forms
+from django.forms.widgets import Widget
 from django.template.loader import render_to_string
 
-class LocationWidget(forms.TextInput):
+class LocationWidget(Widget):
     template_name = 'admin/field_widget_location.html'
 
     def render(self, name, value, attrs=None, renderer=None):
         context = self.get_context(name, value, attrs)
         return render_to_string(self.template_name, context)
+
+    class Media:
+        js = (
+            '/static/js/maplibre-gl.js',
+        )
+
+        css = {
+            'all': (
+                '/static/css/maplibre-gl.css',
+            )
+        }
